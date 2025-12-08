bfc6565435a8fb1d91fefa33c7f545dd7be60f31
riina <54872398+riina@users.noreply.github.com>
2025-12-07 20:38:31 -0800

    Create ArtifactResourceExportOptions.cs

---
+++ src/Art/ArtifactResourceExportOptions.cs
@@ -0,0 +1,21 @@
+﻿namespace Art;
+
+/// <summary>
+/// Specifies options to use when exporting a resource.
+/// </summary>
+public record ArtifactResourceExportOptions
+{
+    /// <summary>
+    /// Default options.
+    /// </summary>
+    public static readonly ArtifactResourceExportOptions Default = new() { IsConcurrent = false };
+
+    /// <summary>
+    /// If true, this export operation is known to be occurring in a concurrent manner with other operations.
+    /// </summary>
+    /// <remarks>
+    /// This property should be used to conditionally disable features that need exclusive control of resources like
+    /// single download progress bars.
+    /// </remarks>
+    public bool IsConcurrent { get; init; }
+}
