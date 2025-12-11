a03f511cf6e65bef84af3b577685777021ee8494
riina <54872398+riina@users.noreply.github.com>
2025-12-10 19:27:49 -0800

    add migration for string limit

--- src/Art.EF.Sqlite/Migrations/20251211032708_AddStringLimits.Designer.cs
+++ src/Art.EF.Sqlite/Migrations/20251211032708_AddStringLimits.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Art.EF.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Art.EF.Sqlite.Migrations
 {
     [DbContext(typeof(SqliteArtifactContext))]
-    partial class ArtifactContextModelSnapshot : ModelSnapshot
+    [Migration("20251211032708_AddStringLimits")]
+    partial class AddStringLimits
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Art.EF.Sqlite/Migrations/20251211032708_AddStringLimits.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Art.EF.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStringLimits : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
