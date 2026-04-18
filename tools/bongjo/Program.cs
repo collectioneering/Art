026f258883f43e3889b6fb862c07f28f8acf3a8e
riina <54872398+riina@users.noreply.github.com>
2026-04-18 01:30:59 -0700

    use bongjo

---
+++ tools/bongjo/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.CommandLine;
+
+var rootCommand = new RootCommand("Support for sarif files and github review comments");
+rootCommand.Add(new ReviewGithubCommand());
+var parseResult = rootCommand.Parse(args);
+parseResult.InvocationConfiguration.Output = Console.Error;
+parseResult.InvocationConfiguration.Error = Console.Error;
+return await parseResult.InvokeAsync();
