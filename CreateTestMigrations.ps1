20525c93a846c180297797f5f98df39aeeda661c
riina <54872398+riina@users.noreply.github.com>
2026-05-28 21:21:04 -0700

    update dependencies

--- CreateTestMigrations.ps1
+++ CreateTestMigrations.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = 'Stop'
-dotnet tool update dotnet-ef -g --version 9.0.14
+dotnet tool update dotnet-ef -g --version 9.0.16
 Remove-Item -Path "$PSScriptRoot/src/Art.EF.Sqlite.Tests.Migrations1/Migrations" -Recurse
 Remove-Item -Path "$PSScriptRoot/src/Art.EF.Sqlite.Tests.Migrations2/Migrations" -Recurse
 dotnet ef migrations add --project "$PSScriptRoot/src/Art.EF.Sqlite.Tests.Migrations1" InitialCreate
